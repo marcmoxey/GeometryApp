5051293a53255da93a80c9636940eb5be9e52cb0
Shadvonic <moxeymarcus28@gmail.com>
2025-03-09 14:18:02 -0400

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeometryLibrary;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double areaOfACircle = GeometryCalculator.CalculateCircleArea(6);
+            Console.WriteLine(areaOfACircle);
+
+            double perimeter = GeometryCalculator.CalculateRectanglePerimeter(5, 10);
+            Console.WriteLine(perimeter);
+
+            double areaOfATriangle = GeometryCalculator.CalculateTriangleArea(3, 4);
+            Console.WriteLine(areaOfATriangle);
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ GeometryLibrary/GeometryCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryLibrary
+{
+    public static class GeometryCalculator
+    {
+        public static double CalculateCircleArea(double r)
+        {
+            double output = 0;
+            output = (Math.PI) * (Math.Pow(r,2));
+            output = Math.Round(output,2);
+            return output;
+        }
+
+        public static double CalculateRectanglePerimeter(double l, double w)
+        {
+            double output = 0;
+            output = 2 * (l + w);
+            return output;
+        }
+
+        public static double CalculateTriangleArea(double b, double h)
+        {
+            double output = 0;
+            output = 0.5 * (b * h);
+            return output;
+        }
+    }
+}
